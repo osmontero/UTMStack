84986ba4d0ddc3615d3c2281a754efb7229eb7d1
Manuel Abascal <mjabascal10@gmail.com>
2025-05-27 11:28:54 -0500

    feat: add minWordsValidator for add rule component

--- frontend/src/app/rule-management/app-rule/validators/customs.validators.ts
+++ frontend/src/app/rule-management/app-rule/validators/customs.validators.ts
@@ -1,10 +1,27 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function containsVariable(variables: string[]): ValidatorFn {
-  console.log('contains variable', variables);
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value || '';
     const hasVariable = variables.some(v => value.includes(v));
     return hasVariable ? null : { noVariableUsed: true };
   };
 }
+
+export function minWordsValidator(minWords: number, minLengthPerWord: number): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value.trim();
+
+    if (!value) { return { minWords: { requiredWords: minWords, minLengthPerWord } }; }
+
+    const words = value
+      .split(/\s+/)
+      .filter(word => word.length >= minLengthPerWord);
+
+    return words.length >= minWords ? null : {
+      minWords: { requiredWords: minWords, minLengthPerWord }
+    };
+  };
+}
+
+
