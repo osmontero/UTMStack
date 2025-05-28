171a56898ee14362f6315a99c9ea6744f3206dac
Manuel Abascal <mjabascal10@gmail.com>
2025-05-28 08:58:18 -0500

    fix: improve CSV export limit parameters

--- frontend/src/app/incident-response/playbooks/playbooks.component.ts
+++ frontend/src/app/incident-response/playbooks/playbooks.component.ts
@@ -1,29 +1,24 @@
-import {HttpResponse} from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
-import {ActivatedRoute} from '@angular/router';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {SourcesService} from '../../admin/sources/sources.service';
+import {Observable, of} from 'rxjs';
+import {catchError, finalize, map, tap} from 'rxjs/operators';
 import {UtmToastService} from '../../shared/alert/utm-toast.service';
 import {
   ModalConfirmationComponent
 } from '../../shared/components/utm/util/modal-confirmation/modal-confirmation.component';
-import {INCIDENT_AUTOMATION_ALERT_FIELDS} from '../../shared/constants/alert/alert-field.constant';
-import {FILTER_OPERATORS} from '../../shared/constants/filter-operators.const';
 import {ITEMS_PER_PAGE} from '../../shared/constants/pagination.constants';
-import {SortEvent} from '../../shared/directives/sortable/type/sort-event';
-import {ElasticOperatorsEnum} from '../../shared/enums/elastic-operators.enum';
+import {UtmAlertType} from '../../shared/types/alert/utm-alert.type';
 import {TimeFilterType} from '../../shared/types/time-filter.type';
-import {IrCreateRuleComponent} from '../shared/component/ir-create-rule/ir-create-rule.component';
 import {IncidentResponseRuleService} from '../shared/services/incident-response-rule.service';
 import {IncidentRuleType} from '../shared/type/incident-rule.type';
+import {PlaybookService} from './playbook.service';
 
 @Component({
   selector: 'app-playbooks',
   templateUrl: './playbooks.component.html',
   styleUrls: ['./playbooks.component.css']
 })
-export class PlaybooksComponent implements OnInit {
+export class PlaybooksComponent implements OnInit, AfterViewInit {
   loading = true;
   rules: IncidentRuleType[];
   range: TimeFilterType;
@@ -31,50 +26,38 @@ export class PlaybooksComponent implements OnInit {
   itemsPerPage = ITEMS_PER_PAGE;
   request = {
     page: 0,
-    size: ITEMS_PER_PAGE,
+    size: 5,
     sort: '',
     'active.equals': null,
     'agentPlatform.equals': null,
     'createdBy.equals': null
   };
-  viewConditions: IncidentRuleType;
   platforms: string[];
   users: string[];
+  platforms$: Observable<string[]>;
+  loadingPlatform = false;
 
-  constructor(private sourcesService: SourcesService,
-              private fb: FormBuilder,
-              private modalService: NgbModal,
+  constructor(private modalService: NgbModal,
               private utmToastService: UtmToastService,
-              private activatedRoute: ActivatedRoute,
-              private incidentResponseRuleService: IncidentResponseRuleService) {
-    this.activatedRoute.queryParams.subscribe(params => {
-      if (params.id) {
-        this.request['id.equals'] = params.id;
-      }
-    });
+              private incidentResponseRuleService: IncidentResponseRuleService,
+              public playbookService: PlaybookService) {
+
   }
 
   ngOnInit() {
-    this.getSelect();
-    this.getRules();
+    this.platforms$ = this.incidentResponseRuleService.getSelectOptions()
+      .pipe(
+        tap(() => this.loadingPlatform = true),
+        map(response => response.body && response.body.users ? response.body.agentPlatform : []),
+        catchError(() => {
+          this.utmToastService.showError('Error', 'An error occurred while fetching platforms.');
+          return of([]);
+        }),
+        finalize(() => this.loadingPlatform = false));
   }
 
-  getSelect() {
-    this.incidentResponseRuleService.getSelectOptions().subscribe(response => {
-      if (response.body) {
-        this.users = response.body.users;
-        this.platforms = response.body.agentPlatform;
-      }
-    });
-  }
-
-  deleteRule(rule) {
-    if (rule) {
-      this.incidentResponseRuleService.delete(rule.id).subscribe(() => {
-        this.utmToastService.showSuccessBottom('Rule ' + rule.name + ' deleted successfully');
-        this.getRules();
-      });
-    }
+  ngAfterViewInit(): void {
+    this.playbookService.loadData({...this.request});
   }
 
   deactivateRuleAction(rule: IncidentRuleType, active: boolean) {
@@ -103,113 +86,40 @@ export class PlaybooksComponent implements OnInit {
 
   loadPage(page: number) {
     this.request.page = page - 1;
-    this.getRules();
+    this.playbookService.loadData({
+      ...this.request,
+    });
   }
 
   onItemsPerPageChange($event: number) {
     this.request.size = $event;
-    this.getRules();
+    this.playbookService.loadData({
+      ...this.request,
+    });
   }
-
-  getRules() {
-    this.incidentResponseRuleService.query(this.request).subscribe(
-      (res: HttpResponse<any>) => this.onSuccess(res.body, res.headers),
-      (res: HttpResponse<any>) => this.onError(res.body)
-    );
-  }
-
-  private onSuccess(data, headers) {
-    this.totalItems = headers.get('X-Total-Count');
-    this.rules = data;
-    this.loading = false;
-  }
-
-  private onError(error) {
-    // this.alertService.error(error.error, error.message, null);
-  }
-
-  onSort($event: SortEvent) {
-    this.request.sort = $event.column + ',' + $event.direction;
-    this.getRules();
-  }
-
 
   searchByRule($event: string | number) {
     this.request['name.contains'] = $event;
-    this.getRules();
-  }
-
-  createRule() {
-    const modal = this.modalService.open(IrCreateRuleComponent, {size: 'lg', centered: true});
-    modal.componentInstance.ruleCreated.subscribe(() => this.getRules());
-  }
-
-  getFieldName(field: string): string {
-    return INCIDENT_AUTOMATION_ALERT_FIELDS.filter(value => value.field === field)[0].label;
-  }
-
-  getFilterName(operator: ElasticOperatorsEnum): string {
-    const index = FILTER_OPERATORS.findIndex(value => value.operator === operator);
-    if (index !== -1) {
-      return FILTER_OPERATORS[index].name;
-    } else {
-      return operator;
-    }
-  }
-
-  editRule(rule: IncidentRuleType) {
-    const modal = this.modalService.open(IrCreateRuleComponent, {size: 'lg', centered: true});
-    modal.componentInstance.rule = rule;
-    modal.componentInstance.ruleCreated.subscribe(() => this.getRules());
-  }
-
-  onUserChange($event: any) {
-    this.request.page = 0;
-    this.request['createdBy.equals'] = $event;
-    this.getRules();
+    this.playbookService.loadData({
+      ...this.request,
+      page: 0,
+    });
   }
 
   onPlatformChange($event: any) {
-    this.request.page = 0;
-    this.request['agentPlatform.equals'] = $event;
-    this.getRules();
-  }
-
-  filterByStatus(active: boolean | null) {
-    this.request.page = 0;
-    this.request['active.equals'] = active;
-    this.getRules();
-  }
-
-  clearFilters() {
-    this.request = {
+    this.playbookService.loadData({
+      ...this.request,
       page: 0,
-      size: ITEMS_PER_PAGE,
-      sort: '',
-      'active.equals': null,
-      'agentPlatform.equals': null,
-      'createdBy.equals': null
-    };
-    this.getRules();
+      'agentPlatform.equals': $event
+    });
   }
 
-  edit(action: IncidentRuleType) {
-
-  }
-
-  remove(action: IncidentRuleType) {
-
-  }
-
-  onSearchInAll($event: string | number) {
-
-  }
-
-  onEdit(item: IncidentRuleType) {
-
-  }
 
   onDelete(item: IncidentRuleType) {
 
+  }
+
+  trackByFn(index: number, playbook: UtmAlertType): any {
+    return playbook.id;
   }
 }
