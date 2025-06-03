5697d582dd1509f81d135b5e358514c618ab5f76
Manuel Abascal <mjabascal10@gmail.com>
2025-06-03 12:20:29 -0500

    feat: restrict access to enterprise modules with upgrade notice

--- frontend/src/app/app-module/shared/components/app-module-card/app-module-card.component.ts
+++ frontend/src/app/app-module/shared/components/app-module-card/app-module-card.component.ts
@@ -1,7 +1,12 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
+import {
+  ModalConfirmationComponent
+} from '../../../../shared/components/utm/util/modal-confirmation/modal-confirmation.component';
 import {VersionType, VersionTypeService} from '../../../../shared/services/util/version-type.service';
+import {ModulesEnterprise} from '../../../services/module.service';
 import {UtmModulesEnum} from '../../enum/utm-module.enum';
 import {UtmModuleType} from '../../type/utm-module.type';
 
@@ -12,15 +17,17 @@ import {UtmModuleType} from '../../type/utm-module.type';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppModuleCardComponent implements OnInit, OnDestroy {
+
+  constructor(private versionTypeService: VersionTypeService,
+              private modalService: NgbModal,) {
+  }
   @Input() module: UtmModuleType;
   @Output() showModuleIntegration = new EventEmitter<UtmModuleType>();
   versionType = VersionType;
   version: VersionType;
   modules = UtmModulesEnum;
   destroy$: Subject<void> = new Subject<void>();
-
-  constructor(private versionTypeService: VersionTypeService) {
-  }
+  ModulesEnterprise = ModulesEnterprise;
 
   ngOnInit() {
     this.versionTypeService.versionType$
@@ -32,9 +39,24 @@ export class AppModuleCardComponent implements OnInit, OnDestroy {
     this.showModuleIntegration.emit(this.module);
   }
 
+  showMessage() {
+    const modalSource = this.modalService.open(ModalConfirmationComponent, {centered: true});
+
+    modalSource.componentInstance.header = 'Enterprise integration';
+    modalSource.componentInstance.message = 'This integration is only available in the Enterprise version of the platform. ' +
+      'If you are interested in accessing this feature or need more information, please contact our support team at ' +
+      '<a href="mailto:support@utmstack.com">support@services.utmstack.com</a>.';
+    modalSource.componentInstance.confirmBtnText = 'Accept';
+    modalSource.componentInstance.confirmBtnIcon = 'icon-cog3';
+    modalSource.componentInstance.confirmBtnType = 'default';
+    modalSource.componentInstance.hideBtnCancel = true;
+    modalSource.result.then(() => {
+
+    });
+  }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
-
 }
