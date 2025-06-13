8f962fdb2a68b075c3d776be0d444fb8e0400ceb
Manuel Abascal <mjabascal10@gmail.com>
2025-06-13 15:24:42 -0500

    fix: set light theme for threat intelligence

--- frontend/src/app/threatwind/threatwind.component.ts
+++ frontend/src/app/threatwind/threatwind.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ThreatwindComponent implements OnInit {
 
   height = window.innerHeight + 250;
-  iframeSrc = 'https://galaxy.threatwinds.com/';
+  iframeSrc = 'https://galaxy.threatwinds.com?theme=light';
 
   constructor() { }
 
