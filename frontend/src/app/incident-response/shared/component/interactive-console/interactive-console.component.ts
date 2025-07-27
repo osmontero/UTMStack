3da988539bedae346aa10ab80bb9dd82d692b3c7
Manuel Abascal <mjabascal10@gmail.com>
2025-07-27 10:41:24 -0500

    feat(interactive-console): enhance agent switching with confirmation modal and improve layout

--- frontend/src/app/incident-response/shared/component/interactive-console/interactive-console.component.ts
+++ frontend/src/app/incident-response/shared/component/interactive-console/interactive-console.component.ts
@@ -1,18 +1,22 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 import {AgentType} from '../../../../shared/types/agent/agent.type';
+import {AgentSidebarService} from '../agent-sidebar/agent-sidebar.service';
 
 @Component({
   selector: 'app-interactive-console',
   templateUrl: './interactive-console.component.html',
-  styleUrls: ['./interactive-console.component.css']
+  styleUrls: ['./interactive-console.component.scss']
 })
 export class InteractiveConsoleComponent implements OnInit {
 
-  @Input() agent: AgentType;
+  agent$: Observable<AgentType>;
 
-  constructor() { }
+  constructor(private agentSidebarService: AgentSidebarService) { }
 
   ngOnInit() {
+    this.agent$ = this.agentSidebarService.selectedAgent$
+      .pipe(filter(agent => !!agent));
   }
-
 }
