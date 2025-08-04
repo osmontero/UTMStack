5a86abbd8adef9c521d431bd2ea9b1e47134806d
Manuel Abascal <mjabascal10@gmail.com>
2025-08-04 11:37:28 -0500

    feat(ModuleAzure): add configuration validation method

--- backend/src/main/java/com/park/utmstack/domain/application_modules/factory/impl/ModuleAzure.java
+++ backend/src/main/java/com/park/utmstack/domain/application_modules/factory/impl/ModuleAzure.java
@@ -1,11 +1,14 @@
 package com.park.utmstack.domain.application_modules.factory.impl;
 
 import com.park.utmstack.domain.application_modules.UtmModule;
+import com.park.utmstack.domain.application_modules.UtmModuleGroupConfiguration;
 import com.park.utmstack.domain.application_modules.enums.ModuleName;
 import com.park.utmstack.domain.application_modules.factory.IModule;
 import com.park.utmstack.domain.application_modules.types.ModuleConfigurationKey;
 import com.park.utmstack.domain.application_modules.types.ModuleRequirement;
+import com.park.utmstack.domain.application_modules.validators.UtmModuleConfigValidator;
 import com.park.utmstack.service.application_modules.UtmModuleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,14 +16,13 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ModuleAzure implements IModule {
     private static final String CLASSNAME = "ModuleAzure";
 
     private final UtmModuleService moduleService;
+    private final UtmModuleConfigValidator utmStackConfigValidator;
 
-    public ModuleAzure(UtmModuleService moduleService) {
-        this.moduleService = moduleService;
-    }
 
     @Override
     public UtmModule getDetails(Long serverId) throws Exception {
@@ -81,5 +83,9 @@ public class ModuleAzure implements IModule {
             .withConfRequired(true)
             .build());
         return keys;
+    }
+
+    public boolean validateConfiguration(UtmModule module, List<UtmModuleGroupConfiguration> configuration) throws Exception {
+        return utmStackConfigValidator.validate(module, configuration);
     }
 }
