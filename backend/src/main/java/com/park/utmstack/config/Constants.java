bea4aec5de70cb40956695ebae9954f16bceb482
Manuel Abascal <mjabascal10@gmail.com>
2025-08-18 12:00:03 -0500

    feat(TFA): add constants for TFA expiration time and issuer

--- backend/src/main/java/com/park/utmstack/config/Constants.java
+++ backend/src/main/java/com/park/utmstack/config/Constants.java
@@ -36,6 +36,8 @@ public final class Constants {
     public static final String PROP_NETWORK_SCAN_API_URL = "utmstack.networkScan.apiUrl";
     public static final String PROP_TFA_ENABLE = "utmstack.tfa.enable";
     public static final String PROP_TFA_TYPE_EMAIL = "utmstack.tfa.method";
+    public static final int EXPIRES_IN_SECONDS = 300; // 5 minutes
+    public static final String TFA_ISSUER = "UTMStack";
 
     // ----------------------------------------------------------------------------------
     // - Modules checks
