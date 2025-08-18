e57a09452e817415c672ce6da132d10c0ea9273f
Manuel Abascal <mjabascal10@gmail.com>
2025-08-18 14:50:55 -0500

    fix: update attack vector translations and adjust chart dimensions for better display

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  SERVER_API_URL: 'https://192.168.1.18/',
-  //SERVER_API_URL: 'http://localhost:8080/',
+  //SERVER_API_URL: 'https://192.168.1.18/',
+  SERVER_API_URL: 'http://localhost:8080/',
   SERVER_API_CONTEXT: '',
   SESSION_AUTH_TOKEN: window.location.host.split(':')[0].toLocaleUpperCase(),
   WEBSOCKET_URL: '//localhost:8080',
@@ -15,7 +15,7 @@ export const environment = {
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
+ * For easier deb`ugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
