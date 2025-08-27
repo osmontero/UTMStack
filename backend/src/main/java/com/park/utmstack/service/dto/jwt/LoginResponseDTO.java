a374dee751d8cd2a6d3a5e339becfcacd4e6ec69
Manuel Abascal <mjabascal10@gmail.com>
2025-08-27 10:22:22 -0500

    feat(TFA): implement LoginResponseDTO and update authentication response to include TFA status

---
+++ backend/src/main/java/com/park/utmstack/service/dto/jwt/LoginResponseDTO.java
@@ -0,0 +1,15 @@
+package com.park.utmstack.service.dto.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class LoginResponseDTO {
+    private boolean success;
+    private boolean tfaRequired;
+    private String method;
+}
+
